0be4900c459e503a2359d2216948d27415254d6f
Ashish Pandey <tech4abetterfuture@gmail.com>
2018-10-11 08:21:09 +0530

    Add files via upload

---
+++ customplugin/provider.go
@@ -0,0 +1,21 @@
+#root of the provider
+
+#The *schema.Provider type describes the provider's properties including:
+
+#the configuration keys it accepts
+#the resources it supports
+#any callbacks to configure
+
+package main
+
+import (
+        "github.com/hashicorp/terraform/helper/schema"
+)
+
+func Provider() *schema.Provider {
+        return &schema.Provider{
+                ResourcesMap: map[string]*schema.Resource{
+				"<provider-name>_server": resourceServer(),
+				},
+        }
+}
